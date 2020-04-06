8a12a0ad3773cba3f4c935c1912a21cea3b85f22
Nikolay Dombrovskiy <ndividl@gmail.com>
2020-04-06 11:03:04 +0700

    Replace Map to es5 array
    Map does not support on IE < 11

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const escapeStringRegexp = require('escape-string-regexp');
 const builtinReplacements = require('./replacements');
 
 const doCustomReplacements = (string, replacements) => {
-	for (const [key, value] of replacements) {
-		if (typeof key !== 'string') return;
+	return replacements.reduce(function (result, replacement) {
+		var key = replacement[0],
+		    value = replacement[1];
+		if (typeof key !== 'string') return result;
 		// TODO: Use `String#replaceAll()` when targeting Node.js 16.
-		string = string.replace(new RegExp(escapeStringRegexp(key), 'g'), value);
-	}
-
-	return string;
+		return result.replace(new RegExp(escapeStringRegexp(key), 'g'), value);
+	}, string);
 };
 
 module.exports = (string, options) => {
@@ -21,14 +21,8 @@ module.exports = (string, options) => {
 	options = Object.assign({
 		customReplacements: [],
 	}, options);
-	
-	const customReplacementsArray = options.customReplacements instanceof Map
-		? Array.from(options.customReplacements.entries())
-		: options.customReplacements;
 
-	const customReplacements = new Map(
-		[].concat(builtinReplacements).concat(customReplacementsArray).filter(Boolean)
-	);
+	const customReplacements = [].concat(builtinReplacements).concat(options.customReplacements).filter(Boolean);
 
 	string = string.normalize();
 	string = doCustomReplacements(string, customReplacements);
